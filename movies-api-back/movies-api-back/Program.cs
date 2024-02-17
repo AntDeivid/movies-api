d172dc3dba54194b3b14a312cc5d755c14a0d6c8
Deivid Santos <deividsantos121@gmail.com>
2024-02-16 23:34:33 -0300

    create entities and db context

--- movies-api-back/movies-api-back/Program.cs
+++ movies-api-back/movies-api-back/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using movies_api_back.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("appsettings.json");
+
+builder.Services.AddDbContext<MoviesDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
